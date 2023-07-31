40c5b2fb8e4bdb48beffcd825e063e4dd7af9c18
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-07-31 20:56:33 +0200

    about style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,24 @@
 import { Col, Row } from "react-bootstrap";
-import fotoLory from "../assets/img/fotoLory.jpg";
+import fotoLory from "../assets/img/fotoLory.jpeg";
+
 const About = () => {
   return (
     <>
       <h2 className="mt-4">About the artist</h2>
-      <Row className="mx-auto my-4">
-        <Col xs={12} md={4} className="mx-auto">
+      <Row xs={8} className="d-flex mx-auto my-4 px-5">
+        <Col xs={12} md={6} className="mx-auto">
           <img
             src={fotoLory}
             alt="foto dell'artista"
-            className="w-50 my-3 mx-auto text-center"
+            className="foto d-flex justify-content-center w-100 my-3 px-3 text-center"
           />
         </Col>
-        <Col xs={12} md={8} className="mx-auto">
-          <p>
+        <Col
+          xs={12}
+          md={6}
+          className="mx-auto ps-xs-5 ps-md-0 py-md-5 w-sx-100 w-md-75"
+        >
+          <p className="ps-4">
             {" "}
             Hello there! I'm Loretana Iacobelli. <br />I started painting as a
             hobby but now it has become my greatest passione.
