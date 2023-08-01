80772d3cf4dfbd2a37b0ed750a020066aac21837
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-08-01 10:06:15 +0200

    blog style v_0.0.2

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,14 +3,25 @@ import { Container, Row, Col } from "react-bootstrap";
 const Blog = () => {
   return (
     <>
-      <h2 className="mt-4"> What's new? </h2>
+      <h2 className="mt-4 mb-5"> What's new? </h2>
       <Container>
-        <Row className="d-flex flex-row"> title </Row>
-        <Col> description </Col>
-        <Col>where</Col>
-        <Col>when</Col>
+        <Row
+          xs={11}
+          md={9}
+          className=" px-auto d-flex flex-column border border-info align-content-space-around mx-1"
+        >
+          {" "}
+          <h4 className="text-center">New exhibit!</h4>
+          <Col md={6}>
+            {" "}
+            I am thrilled to invite you to my next exhibit. Here you'll have the
+            chace to see my paintings in vivo. <br />
+            See you there! <hr />
+          </Col>
+          <Col md={6}>Bari - via Putignani 79</Col>
+          <Col md={6}>15/10/2023</Col>
+        </Row>
       </Container>
-      ;
     </>
   );
 };
